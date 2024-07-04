75b04954ac2939eaacfc84512c58af2eb095677f
rukije <rukije.morina@harrisia.com>
2024-07-04 11:11:37 +0200

    Added rawer Navigation, and app last finished touches

---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,10 @@
+import {View,Text} from "react-native";
+
+function FavoriteScreen() {
+    return(
+        <View>
+            <Text>FavoriteScreen</Text>
+        </View>
+    )
+}
+export default FavoriteScreen;
